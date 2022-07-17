e13cdee27ad778f6b8ef08b5f4296672475cca8e
Lucas Thimoteo <lucasthim1@gmail.com>
2022-07-16 21:07:19 -0300

    task 03 development

--- notebooks/data_access_handler.py
+++ notebooks/data_access_handler.py
@@ -5,8 +5,14 @@ class DataAccessHandler():
 
     """ Class that enables access to the dataset."""
     
-    def __init__(self,main_path) -> None:
+    def __init__(self,main_path:str) -> None:
+        """
+        Params:
+
+        main_path: local path to load data or save data.
         
+        """
+
         self.main_path = main_path if (main_path[-1] == "/") else main_path + "/"
 
     def load(self, dataset_type:str) -> pd.DataFrame:
@@ -17,5 +23,5 @@ class DataAccessHandler():
     def save(self, df:pd.DataFrame, dataset_type:str) -> None:
         """Save dataset into main_path directory. dataset_type parameter is to save specific sets, such as train and test."""
         dataset_name = f"fetal_health.csv" if dataset_type == "" else f"fetal_health_{dataset_type}.csv"
-        df.to_csv(self.main_path + dataset_name)
+        df.to_csv(self.main_path + dataset_name,index=False)
 
